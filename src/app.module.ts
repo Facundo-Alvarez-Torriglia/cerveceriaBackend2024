ef28cc04b04e0d3f95e9571068400b5e37336913
andres-coda <codaandres@gmail.com>
2024-04-18 20:10:57 -0300

    Instalacion de dependiencias completa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'app') }),
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'cerveceria',
+    entities: [__dirname + "/entity/*{.js,.ts}"], //__dirname + "/entity/*{.js,.ts}"
+    synchronize: false,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
