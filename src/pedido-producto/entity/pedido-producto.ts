10e2d926e3db723366a406e1426d1b99add90be0
andres-coda <codaandres@gmail.com>
2024-04-30 20:02:47 -0300

    borrado en cascada productos pedido, y super relaciones

--- src/pedido-producto/entity/pedido-producto.ts
+++ src/pedido-producto/entity/pedido-producto.ts
@@ -1,7 +1,6 @@
 import { Pedido } from "src/pedido/entity/pedido.entity";
 import { Producto } from "src/producto/entidad/Producto.entity";
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn  } from "typeorm";
-import { JoinAttribute } from "typeorm/query-builder/JoinAttribute";
 
 
 @Entity()
@@ -13,11 +12,11 @@ export class PedidoProducto {
     @Column({ type: Number, nullable: false, default: 0 })
     cantidad: number;
 
-    @ManyToOne(()=> Pedido, pedido=> pedido.pedidosProducto)
+    @ManyToOne(()=> Pedido, pedido=> pedido.pedidosProducto, {cascade: true})
     @JoinColumn({name:"idPedido"})
     pedido: Pedido;
 
-    @ManyToOne(()=> Producto, producto=> producto.pedidoProductos)
+    @ManyToOne(()=> Producto, producto=> producto.pedidoProductos,{cascade: true})
     @JoinColumn({ name:"idProducto" })
     producto:Producto;
 }
